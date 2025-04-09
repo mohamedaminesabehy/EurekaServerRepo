a853e78edb072cb5a2d234aa9f2563780eba9d3a
mohamedaminesabehy <110574315+mohamedaminesabehy@users.noreply.github.com>
2025-04-09 13:54:05 +0100

    message

--- Restaurant/src/main/java/tn/esprit/microserviceproject/repositories/restauRepository.java
+++
@@ -1,9 +0,0 @@
-package tn.esprit.microserviceproject.repositories;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tn.esprit.microserviceproject.entities.Restaurant;
-
-public interface restauRepository extends JpaRepository<Restaurant, Long> {
-
-}
--- microserviceproject/src/main/java/tn/esprit/microserviceproject/repositories/menuRepository.java
+++
@@ -1,7 +0,0 @@
-package tn.esprit.microserviceproject.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tn.esprit.microserviceproject.entities.Menu;
-
-public interface menuRepository extends JpaRepository<Menu, Long> {
-}
